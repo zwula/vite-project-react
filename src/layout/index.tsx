a5481f099728afd52fb04541d03ccd5607ddf0d3
岳磊 <348468470@qq.com>
2023-09-03 22:47:00 +0800

    feat: 新增AppLayout视图容器组件

---
+++ src/layout/index.tsx
@@ -0,0 +1,29 @@
+/*
+ ** 仅仅是一个布局容器，和路由没有直接关系，和路由相关的视图都在views文件夹下
+ */
+
+import { Layout } from 'antd'
+import { Outlet } from 'react-router-dom'
+import './index.less'
+
+const { Header, Footer, Sider, Content } = Layout
+
+const classPrefix = 'app-layout'
+const AppLayout = () => {
+	return (
+		<div>
+			<Layout className={classPrefix}>
+				<Sider className={`${classPrefix}-sider`}>Sider</Sider>
+				<Layout>
+					<Header className={`${classPrefix}-header`}>Header</Header>
+					<Content className={`${classPrefix}-content`}>
+						<Outlet />
+					</Content>
+					<Footer className={`${classPrefix}-footer`}>Footer</Footer>
+				</Layout>
+			</Layout>
+		</div>
+	)
+}
+
+export default AppLayout
